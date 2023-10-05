2b69edf3d1ed4320bec951efaeac993cdfbdf1f7
Momox <119704058+momoox@users.noreply.github.com>
2023-10-05 19:34:05 +0200

    Add files via upload

--- print_list.c
+++ print_list.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_list.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: oliove <olivierliove@student.42.fr>        +#+  +:+       +#+        */
+/*   By: momox <momox@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/30 16:06:13 by momox             #+#    #+#             */
-/*   Updated: 2023/10/05 18:35:04 by oliove           ###   ########.fr       */
+/*   Updated: 2023/10/05 19:33:40 by momox            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,11 @@ void	print_list(t_list *list)
 		printf("	NULL\n");
 		return ;
 	}
-	printf("\033[0;34m=====PRINT LIST=====\033[0m\n");
-	while (list)
-	{
+	// printf("\033[0;34m=====PRINT LIST=====\033[0m\n");
+	// while (list)
+	// {
 
-		printf("\033[0;34mList\033[0m\n");
+		// printf("\033[0;34mList\033[0m\n");
 		printf("	list->content = \033[0;32m[%s]\033[0m\n", list->content);
 		printf("	list->Token = \033[0;31m[%d]\033[0m\n", list->token);
 		if(list->cmd) 
@@ -34,9 +34,9 @@ void	print_list(t_list *list)
 		// 	printf("tab 0 = %s\n", list->cmd[0]);
 		// 	printf("tab 1 = %s\n", list->cmd[1]);
 		// }
-		list = list->next;
-	}
-	printf("\033[0;35m	====================\033[0m\n\n");
+		// list = list->next;
+	// }
+	// printf("\033[0;35m	====================\033[0m\n\n");
 }
 		// if (list->next->next == NULL)
 		// printf("list->token = %d\n", list->token);
